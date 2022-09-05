df90ce008590958b14cf67f9ab8473063e0bd9c2
giomen88 <giorgia9288@virgilio.it>
2022-09-05 20:05:35 +0200

    snack2 ok

--- snack2.php
+++ snack2.php
@@ -3,18 +3,20 @@
 // che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
 // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”;
 
+$name = $_GET['name'] ?? '';
+$email = $_GET['email'] ?? '';
+$age = $_GET['age'] ?? '';
 
-?>
+//validazione
+$is_age_valid = is_numeric($age);
+$is_name_valid = mb_strlen(trim($name)) > 3;
+$is_email_valid = strpos($email, '@') && strpos($email, '.');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+if ($is_age_valid && $is_email_valid && $is_name_valid) {
+    echo 'Mail valida';
+} else {
+    echo 'Mail non valida';
+}
+
+
+
